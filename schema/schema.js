a46d678d497d7cb2aca4cafc2cd59f95971ec35e
Prodigy00 <simplygiddy@gmail.com>
2019-06-30 22:48:18 +0100

    Building graphql schema, built UserType

---
+++ schema/schema.js
@@ -0,0 +1,12 @@
+import { GraphQLInt } from "graphql";
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  }
+});

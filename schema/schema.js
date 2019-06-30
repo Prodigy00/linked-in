0b0422c6b674ec590c04462ad035b76995cffa18
Prodigy00 <simplygiddy@gmail.com>
2019-06-30 23:11:02 +0100

    created hard-coded schema

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,12 @@
 import { GraphQLInt } from "graphql";
+const _ = require("lodash"); //helper library, helps  us walk through collections of data
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+
+const users = [
+  { id: "45", firstName: "David", age: 29 },
+  { id: "31", firstName: "Dozie", age: 31 }
+];
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -9,4 +15,27 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt }
   }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    //connect to user type
+    user: {
+      type: UserType,
+      args: {
+        id: { type: GraphQLString },
+        //where we go into our data store
+        resolve(parentValue, args) {
+          //walking through our list of users, find and return the first user who ha an id equal to args.id
+          return _.find(users, { id: args.id });
+        }
+      }
+    }
+  }
+});
+
+//GraphQL schema turns a root query into a schema instance
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 
 const expressGraphQL = require("express-graphql");
 
+const schema = require("./schema/schema.js");
+
 const app = express();
 
 app.use(
   "/graphql",
   expressGraphQL({
+    schema, //aka schema:schema
     graphiql: true
   })
 );
